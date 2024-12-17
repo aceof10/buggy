606cdd20bf6335c71297044c4e43b98a031666aa
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-17 16:52:23 +0300

    update app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import db from "./models/index.js";
 
 import helloRoutes from "./routes/helloRoutes.js";
+import authRutes from "./routes/authRoutes.js";
 
 const app = express();
 
 app.use(bodyParser.json());
 
 app.use("/hello", helloRoutes);
+app.use("/auth", authRutes);
+
+db.sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Database connected.");
+    return db.sequelize.sync();
+  })
+  .catch((err) => {
+    console.log("Unable to connect to the database.");
+  });
 
 export default app;
