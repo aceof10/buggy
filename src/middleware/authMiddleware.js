80bd2d3ba92b39382a560d47d66de9df3ca45a55
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-19 22:07:19 +0300

    add user existence check in authentication middleware to validate user from token

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,26 @@
 import { verifyAccessToken } from "../utils/jwtUtil.js";
+import { userExistsInDb } from "../utils/userExistsInDbUtil.js";
 
-export const authenticate = (req, res, next) => {
+export const authenticate = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ message: "Not authenticated." });
+    return res.status(401).json({ message: "Unauthorized." });
   }
 
   try {
     const decoded = verifyAccessToken(token);
+
+    const userIdInToken = decoded.userId;
+
+    const userExists = await userExistsInDb({ id: userIdInToken });
+    if (!userExists) {
+      return res.status(401).json({ message: "User does not exist." });
+    }
+
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: "Invalid or expired token." });
+    return res.status(401).json({ message: error.message });
   }
 };
