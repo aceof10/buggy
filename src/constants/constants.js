b84059c34762f578212af930d424f0619037a3d6
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-23 16:52:29 +0300

    Feat: add constants.js

---
+++ src/constants/constants.js
@@ -0,0 +1,14 @@
+/**
+ * ROLES constants
+ */
+export const USER = "user";
+export const DEVELOPER = "developer";
+export const TESTER = "tester";
+export const ADMIN = "admin";
+
+export const ROLES_LIST = [USER, DEVELOPER, TESTER, ADMIN];
+export const ROLES_AUTHORIZED_FOR_PROJECT_ASSIGNMENT = [
+  DEVELOPER,
+  TESTER,
+  ADMIN,
+];
