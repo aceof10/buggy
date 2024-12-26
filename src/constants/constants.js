59e4ad33b6abf7ceed8f677711fcff9537450b71
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-27 00:14:19 +0300

    Feat: add status and priority constants for projects and bugs

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -12,3 +12,49 @@ export const ROLES_AUTHORIZED_FOR_PROJECT_ASSIGNMENT = [
   TESTER,
   ADMIN,
 ];
+
+/**
+ * STATUS constants
+ */
+export const NEW = "new";
+export const OPEN = "open";
+export const DUPLICATE = "duplicate";
+export const NOT_REPRODUCIBLE = "not_reproducible";
+export const NOT_A_BUG = "not_a_bug";
+export const IN_PROGRESS = "in_progress";
+export const ON_HOLD = "on_hold";
+export const RESOLVED = "resolved";
+export const CLOSED = "closed";
+export const CANCELLED = "cancelled";
+export const REOPENED = "reopened";
+
+export const PROJECT_STATUS_LIST = [
+  NEW,
+  OPEN,
+  ON_HOLD,
+  CLOSED,
+  CANCELLED,
+  REOPENED,
+];
+
+export const BUG_STATUS_LIST = [
+  NEW,
+  OPEN,
+  DUPLICATE,
+  NOT_REPRODUCIBLE,
+  NOT_A_BUG,
+  IN_PROGRESS,
+  ON_HOLD,
+  RESOLVED,
+  CLOSED,
+  REOPENED,
+];
+
+/**
+ * PRIORITY constants
+ */
+export const LOW = "low";
+export const MEDIUM = "medium";
+export const HIGH = "high";
+
+export const PRIORITY_LIST = [LOW, MEDIUM, HIGH];
