2fa947183b29ac7aa43463aff4dada05ccf7dad9
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-27 16:28:29 +0300

    Refactor: refactor authMiddleware to return better error statuses and messages
    added TODO comment

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,25 +2,40 @@ import { verifyAccessToken } from "../utils/jwtUtil.js";
 import { userExistsInDb } from "../utils/userExistsInDbUtil.js";
 
 export const authenticate = async (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-
-  if (!token) {
-    return res.status(401).json({ message: "Unauthorized." });
-  }
-
   try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader?.startsWith("Bearer ")) {
+      return res
+        .status(401)
+        .json({ message: "Malformed authorization header." });
+    }
+
+    const token = authHeader.split(" ")[1];
     const decoded = verifyAccessToken(token);
 
     const userIdInToken = decoded.userId;
-
     const userExists = await userExistsInDb({ id: userIdInToken });
     if (!userExists) {
-      return res.status(401).json({ message: "User does not exist." });
+      /*
+      Valid token for non-existent user.
+      Potential security issue because this would either mean
+      JWT_ACCESS_SECRET is exposed hence can be used to generate JWTs that pass validation
+      or user is deleted from database, thus need for invalidating their previous JWT token(s)
+      */
+
+      /*
+      TODO
+      - Log for investigation
+      - Revoke existing tokens (to invalidate tokens for deleted users)
+      - Trigger alerts to investigate possible key leaks
+      */
+
+      return res.status(401).json({ message: "Unauthorized." });
     }
 
     req.user = decoded;
     next();
   } catch (error) {
-    return res.status(401).json({ message: error.message });
+    return res.status(401).json({ message: "Unauthorized." });
   }
 };
