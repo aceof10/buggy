b0f98352de3ede3b75bf11db15b89121a89a071c
Bradley Mwangangi <177125727+aceof10@users.noreply.github.com>
2024-12-30 14:37:33 +0300

    refactor: add function to handle JWT verification with error handling

--- src/utils/jwtUtil.js
+++ src/utils/jwtUtil.js
@@ -1,5 +1,27 @@
 import jwt from "jsonwebtoken";
 
+const handleJWTVerification = (token, secret) => {
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    const err = new Error();
+    switch (error.name) {
+      case "TokenExpiredError":
+        err.message = "Session expired. Please log in again.";
+        err.status = 401;
+        break;
+      case "JsonWebTokenError":
+        err.message = "Invalid session. Please log in again.";
+        err.status = 401;
+        break;
+      default:
+        err.message = "Internal server error.";
+        err.status = 500;
+    }
+    throw err;
+  }
+};
+
 export const generateAccessToken = (userId, role) => {
   return jwt.sign({ userId, role }, process.env.JWT_ACCESS_SECRET, {
     expiresIn: "15m",
@@ -13,9 +35,9 @@ export const generateRefreshToken = (userId) => {
 };
 
 export const verifyAccessToken = (token) => {
-  return jwt.verify(token, process.env.JWT_ACCESS_SECRET);
+  return handleJWTVerification(token, process.env.JWT_ACCESS_SECRET);
 };
 
 export const verifyRefreshToken = (token) => {
-  return jwt.verify(token, process.env.JWT_REFRESH_SECRET);
+  return handleJWTVerification(token, process.env.JWT_REFRESH_SECRET);
 };
